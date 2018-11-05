0bd3da9b37fa5f80a544d770b5933c5a35e25083
pingguoshouji <31036366+pingguoshouji@users.noreply.github.com>
2018-11-05 22:36:18 +0800

    __init__.py
    init工程详解

---
+++ a/a.py
@@ -0,0 +1,6 @@
+# from b import *
+import b.b
+print(b.b.c)
+# print(a)
+
+
---
+++ a/b/Student.py
@@ -0,0 +1,19 @@
+class Student():
+    a = 1
+
+    def __init__(self,name,age,a):
+        self.name = name
+        self.age = age
+        self.a = a 
+        # print(Student.a)
+        # print(self.__class__.a)
+
+    def print_a(self):
+        print(self.a)
+        print(self.__class__.a)
+        print(self.name)
+
+student1 = Student('YUANZHE',18,2)
+student1.print_a()
+# print(student1.name)
+# print(student1.__dict__)
