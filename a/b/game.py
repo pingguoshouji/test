f83f323732fa9df017f8a794c615c16afbfd19e5
jimmy <1783762627@qq.com>
2018-11-06 17:18:57 +0800

    很蠢的游戏

---
+++ a/b/game.py
@@ -0,0 +1,24 @@
+# 一个猜数字的蠢游戏
+
+from random import randint
+
+a = randint(1,100)
+print(a)
+
+b = int(input())
+
+# while a != b:
+#     if a > b:
+#         print("big")
+#     else:
+#         print("small")
+
+# print("bingo")
+
+if a != b:
+    if a>b:
+        print
+    
+else:
+    print("bingo")
+    
