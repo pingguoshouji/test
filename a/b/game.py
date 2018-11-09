a79dcbb338ac72da68071b28c42183ea0846476d
jimmy <1783762627@qq.com>
2018-11-09 17:54:39 +0800

    point

--- a/b/game.py
+++ a/b/game.py
@@ -2,7 +2,7 @@
 
 from random import randint
 
-<<<<<<< HEAD
+
 num = randint(1,100)
 # print(num)
 
@@ -24,25 +24,5 @@ while a != True:
     # print(num > b)
     # print(b)
     
-=======
-a = randint(1,100)
-print(a)
 
-b = int(input())
 
-# while a != b:
-#     if a > b:
-#         print("big")
-#     else:
-#         print("small")
-
-# print("bingo")
-
-if a != b:
-    if a>b:
-        print
-    
-else:
-    print("bingo")
-    
->>>>>>> f83f323732fa9df017f8a794c615c16afbfd19e5
---
+++ a/b/point.py
@@ -0,0 +1,11 @@
+
+for a in range(0,5):
+    for b in range(0,5):
+        if b <= a:
+            print('*',end='')
+        else:
+            print('*')
+
+    
+
+
