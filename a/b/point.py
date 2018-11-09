4ec0acbca238c5d26a13acb52a55f9141c3f5954
pingguoshouji <31036366+pingguoshouji@users.noreply.github.com>
2018-11-09 23:10:59 +0800

    new

--- a/b/point.py
+++ a/b/point.py
@@ -1,12 +1,19 @@
+# 三角
+# for a in range(1,5):
+#     for b in range(1,5):
+#         if b <= a - 1:
+#             print('*',end='')
+#         else:
+#             print('*')
+#             break
 
-for a in range(1,5):
-    for b in range(1,5):
-        if b <= a - 1:
-            print('*',end='')
-        else:
-            print('*')
-            break
+for i in range(0,5):
+    for j in range(0,i-1):
+        print('*',end = '')
+    print()
 
-    
-
-
+# 矩阵
+# for a in range(0,5):
+#     for b in range(0,5):
+#         print('*',end='')
+#     print()
---
+++ a/b/round.py
@@ -0,0 +1,3 @@
+for a in range(0,4):
+    for b in range(0,4):
+        print(a,b)
