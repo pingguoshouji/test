b20a7ecb109f9845ef8c32e763febb961a28ead2
jimmy <1783762627@qq.com>
2018-11-12 17:53:02 +0800

    new

---
+++ a/b/list nesting.py
@@ -0,0 +1,16 @@
+
+a = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+]
+
+# [[row[i] for row in a] for i in range(4)]
+
+# # print(a)
+
+# [[t[i] for t in a] for i in range(4)]
+
+# print(a)
+
+[print(i) for i in a]
