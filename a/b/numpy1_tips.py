dfe1649cd4a495320a7651e3b3b08f954cd6e878
jimmy <1783762627@qq.com>
2018-12-10 19:53:40 +0800

    new

--- a/b/numpy1_tips.py
+++ a/b/numpy1_tips.py
@@ -17,5 +17,27 @@ for i in range(8):
     arr[i] = i
 
 print(arr[[4, 3, 0, 6]])
-print(arr([4, 3, 0, 6]))
+
+# 将条件逻辑表述为数组运算
+import numpy as np
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5])
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
+cond = np.array([True, False, True, True, False])
+
+result1 = [(x if c else y)for x, y, c in zip(xarr, yarr, cond)]
+result2 = np.where(cond, xarr, yarr)
+
+print(result2)
+
+import numpy as np
+
+arr2 = np.random.randn(5,4)
+print(arr2)
+print(arr2.sum(axis = 2))               # numpy.core._internal.AxisError: axis 2 is out of bounds for array of dimension 2
+
+
+import numpy as np
+
+arr3 = np.random.randn(100)
+print((arr3>0).sum())
 
