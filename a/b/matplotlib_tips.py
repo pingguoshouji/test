1c066465257028da416d20a5085eb64eace343a0
jimmy <1783762627@qq.com>
2018-12-29 17:37:29 +0800

    new

---
+++ a/b/matplotlib_tips.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+plt.plot(np.random.rand(100))
+plt.show()    
+
+x = np.random.randn(100)
+y = np.random.randn(100)
+plt.scatter(x,y)
+plt.show()
+
